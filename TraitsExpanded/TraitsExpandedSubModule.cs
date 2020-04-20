f30f98a87baa95d9ff2e938e8e347d626e46b43b
Jonas Rotermund <j.rotermund@live.de>
2020-04-20 20:39:20 +0200

    Created SubModule base

---
+++ TraitsExpanded/TraitsExpandedSubModule.cs
@@ -0,0 +1,26 @@
+﻿using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.CampaignSystem;
+
+namespace TraitsExpanded
+{
+    public class TraitsExpandedSubModule : MBSubModuleBase
+    {
+
+		/* Executed when game is loaded or initially started */
+		protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+		{
+			if (game.GameType is Campaign)
+			{
+				CampaignGameStarter gameInitializer = (CampaignGameStarter)gameStarterObject;
+				this.AddBehaviors(gameInitializer);
+			}
+		}
+
+		/* The Behaviors we add */
+		private void AddBehaviors(CampaignGameStarter gameStarterObject)
+		{
+			//gameStarterObject.AddBehavior(new OurBehavior());
+		}
+	}
+}
