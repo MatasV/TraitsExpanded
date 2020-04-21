8e712674b2c218728f08c3218082b5922c7497d0
Matas V <matusisga@gmail.com>
2020-04-22 00:10:23 +0300

    Continuing the Behaviour logic
    Added the timers for ticking the delegates. Added some basic searches and some name refactoring to make more sense, since we no longer use Heros, instead we use CharacterObjects

--- TraitsExpanded/TraitSystemBehavior.cs
+++ TraitsExpanded/TraitSystemBehavior.cs
@@ -1,5 +1,5 @@
 ﻿using System.Collections.Generic;
-
+using System.Linq;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.SandBox.CampaignBehaviors;
 using TaleWorlds.Core;
@@ -10,7 +10,7 @@ namespace TraitsExpanded
     public class TraitSystemBehavior : CampaignBehaviorBase
     {
         public static readonly List<ITraitSet> AllTraitSets = new List<ITraitSet>();
-        public static Dictionary<Hero, List<ITraitSet>> HeroTraitInfo = new Dictionary<Hero, List<ITraitSet>>();
+        public static Dictionary<CharacterObject, List<ITraitSet>> CharacterTraitInfo = new Dictionary<CharacterObject, List<ITraitSet>>();
 
         public delegate void CampaignTickCall(CampaignStatusInfo statusInfo);
 
@@ -20,9 +20,13 @@ namespace TraitsExpanded
 
         public static MissionTickCall OnMissionTick;
 
-        private static float MissionTickTime { get; set; } = 1f;
-        private static float CampaignTickTime { get; set; } = 1f;
-        private static float GameTickTime { get; set; } = 1f;
+        public delegate void GameTickCall(Game statusInfo);
+
+        public static GameTickCall OnGameTick;
+
+        private static float MissionTickTime { get; } = 1f;
+        private static float CampaignTickTime { get; } = 1f;
+        private static float GameTickTime { get; } = 1f;
 
         private Timer MissionTickTimer { get; } = new Timer(Time.ApplicationTime, MissionTickTime, false);
         private Timer CampaignTickTimer { get; } = new Timer(Time.ApplicationTime, CampaignTickTime, false);
@@ -32,81 +36,120 @@ namespace TraitsExpanded
             Init();
         }
 
-        private static void Init()
+        private void Init()
         {
-            
+            OnCampaignTick += ValidateHeroList;
         }
 
-        public static void Tick()
+        public void Tick()
         {
-            
+            if (MissionTickTimer.Check(Time.ApplicationTime))
+            {
+                OnMissionTick.Invoke(new MissionStatusInfo());
+                MissionTickTimer.Reset(Time.ApplicationTime, MissionTickTime);
+            }
+            if (CampaignTickTimer.Check(Time.ApplicationTime))
+            {
+                OnCampaignTick.Invoke(new CampaignStatusInfo());
+                CampaignTickTimer.Reset(Time.ApplicationTime, CampaignTickTime);
+            }
+            if (GameTickTimer.Check(Time.ApplicationTime))
+            {
+                OnGameTick.Invoke(Game.Current);
+                GameTickTimer.Reset(Time.ApplicationTime, MissionTickTime);
+            }
         }
 
         public void CheckRequirements()
         {
             
         }
+
+        private void ValidateHeroList(CampaignStatusInfo statusInfo)
+        {
+            var characterObjects = new List<CharacterObject>();
+            
+            foreach (var character in statusInfo.CurrentCampaign.Characters)
+            {
+                if (CharacterTraitInfo.ContainsKey(character))
+                {
+                    characterObjects.Add(character);
+                }
+                else
+                {
+                    //LoadCharacterTraitData(character);
+                    //adding this hero to the List manually for now, otherwise, the addition to the list should be moved to loadCharacterTraitData and GenerateCharacterTraits
+                    characterObjects.Add(character);
+                    CharacterTraitInfo.Add(character, new List<ITraitSet>());
+                }
+            }
+
+            foreach (var oldCharacterObject in CharacterTraitInfo.Keys
+                .Where(oldCharacterObject => characterObjects
+                    .All(freshCharacterObject => oldCharacterObject != freshCharacterObject)))
+            {
+                //should save the character traits at this point since we will be removing them from the active Character List
+                CharacterTraitInfo.Remove(oldCharacterObject);
+            }
+        }
         
-        
-        
-        private bool AddTrait(Hero hero, ITraitSet traitSet)
+        private bool AddTraitSet(CharacterObject character, ITraitSet traitSet)
+        {
+            return true;
+        }
+        private bool AddTrait(CharacterObject character, ITrait traitSet)
+        {
+            return true;
+        }
+        private bool LoadCharacterTraitData(CharacterObject character)
+        {
+            //if the data exists, load it and return true, else call GenerateCharacterTraits()
+            
+            return true;
+        }
+
+        private bool SaveCharacterTraitData(CharacterObject character)
         {
             return true;
         }
 
-        private bool LoadHeroTraitData(Hero hero)
+        private void GenerateCharacterTraits(CharacterObject character)
         {
-            return true;
-        }
-
-        private bool SaveHeroTraitData(Hero hero)
-        {
-            return true;
-        }
-
-        private void GenerateHeroTraits(Hero hero)
-        {
+            //generate traits and add them to the CharacterTraitInfo
         }
 
         public List<ITrait> GetAllPositiveTraits()
         {
-            return new List<ITrait>();
+            return (from traitSet in AllTraitSets from 
+            trait in traitSet.Traits where trait.Cost >= 0 select trait).ToList();
         }
 
         public List<ITrait> GetAllNegativeTraits()
         {
-            return new List<ITrait>();
+            return (from traitSet in AllTraitSets from 
+                trait in traitSet.Traits where trait.Cost < 0 select trait).ToList();
         }
 
-        private void TickMission()
-        {
-        }
-
-        private void TickCampaign()
-        {
-        }
-
-        public void RegisterTraitSet(ITraitSet set, List<Hero> heroesToGiveTraitTo = null)
+        public void RegisterTraitSet(ITraitSet set, List<CharacterObject> charactersToGiveTraitTo = null)
         {
             if (!AllTraitSets.Contains(set))
             {
                 AllTraitSets.Add(set);
-                if (heroesToGiveTraitTo != null)
+                if (charactersToGiveTraitTo == null) return;
+                
+                foreach (var character in charactersToGiveTraitTo)
                 {
-                    foreach (var hero in heroesToGiveTraitTo)
+                    if(CharacterTraitInfo.TryGetValue(character, out var currentSet))
                     {
-                        if(HeroTraitInfo.TryGetValue(hero, out var currentSet))
+                        if (!currentSet.Contains(set))
                         {
-                            if (!currentSet.Contains(set))
-                            {
-                                currentSet.Add(set);
-                            }
+                            currentSet.Add(set);
                         }
-                        else
-                        {
-                            //generate a new Hero with traits or return that the hero does not exist
-                        }
-                    }       
+                    }
+                    else
+                    {
+                        //generate a new Character with traits or return that the hero does not exist
+                    }
                 }
             }
         }
